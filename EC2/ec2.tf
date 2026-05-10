70997b35e78d5a03ef535e90128e7dffae87e97e
sai <saiteja121020@gmail.com>
2026-05-09 21:03:12 -0400

    Action

--- EC2/ec2.tf
+++ EC2/ec2.tf
@@ -1,7 +1,7 @@
 resource "aws_instance" "example" {
   ami                 = "ami-0220d79f3f480ecf5"
   instance_type       = "t3.micro"
-  vpc_security_groups_ids = [aws_security_group.allow_tls.id]
+  vpc_security_group_ids = [aws_security_group.allow_tls.id]
 
   tags = {
     Name    = "terraform-state-demo"
